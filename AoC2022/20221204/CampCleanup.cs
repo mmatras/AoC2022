235960d0397d1e9076554ff5d807bf2e0330d2e1
Daniel Matras <mmatras@gmail.com>
2022-12-04 11:27:08 +0100

    day four

---
+++ AoC2022/20221204/CampCleanup.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+
+namespace AoC2022._20221204
+{
+    internal static class CampCleanup
+    {
+        public static void Run()
+        {
+            var lines = File.ReadAllLines("20221204/input.txt");
+            
+            var input = lines.Select(l => l.Split(new[] { '-', ',' }).Select(ch => int.Parse(ch)).ToList());
+            
+            var fullyOverlap = input.Count(el => el[0] <= el[2] && el[1] >= el[3] || 
+                             el[2] <= el[0] && el[3] >= el[1]);
+
+            Console.WriteLine($"fully contain the other: {fullyOverlap}");
+
+            var overlap = input.Count(el => el[2] <= el[1] && el[3] >= el[0]);
+
+            Console.WriteLine($"ranges overlap: {overlap}");
+
+        }
+    }
+}
--- AoC2022/Program.cs
+++ AoC2022/Program.cs
@@ -1,4 +1,5 @@
 ﻿//DayOne
 //AoC2022._20221201.CalorieCounting.Run();
 //AoC2022._20221202.RockPaperScissors.Run(); 
-AoC2022._20221203.RucksackReorganization.Run();
+//AoC2022._20221203.RucksackReorganization.Run();
+AoC2022._20221204.CampCleanup.Run();
